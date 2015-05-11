8c099360dd47bbf1f13d9b68339daffe6fa2a6e3
phobos101 <kRuiden1er>
2015-05-11 12:10:34 +0100

    Added item descriptions (need to figure out how to keep items independant to move over description also if user picks it up
    Fixed area name
    
    Altered rooms to locations
    
     Added character sheet
    
     Altered behaviour of typing unsupported command

--- game.py
+++ game.py
@@ -1,66 +1,87 @@
 __author__ = 'Robert Wilson - robwilson101@gmail.com'
 
 def showInstructions():
-    print ("RPG Game")
+    print ("Master of Avalon")
     print ("========")
     print ("Commands:")
     print ("'Go [direction]' e.g 'Go east'")
     print ("'Get [item]' e.g 'Get Sword")
     print ("'Map' to see where you can go")
     print ("'Inventory' to see your inventory")
+    print ("'Sheet' to see your character sheet (HP, mana, etc)")
+    print ("Other commands such as 'eat', 'drink', 'look' are also supported. Experiment!")
 
 def showStatus():
     #prints out current status
-    print ("\nYou are currently in the %r" % rooms[currentRoom]["name"])
-    if "item" in rooms[currentRoom]:
-        print ("You see a " + rooms[currentRoom]["item"])
+    print ("\nYou are currently in the %r" % location[currentLocation]["name"])
+    if "item*" in location[currentLocation]:
+        print ("You see a " + location[currentLocation]["item"]["name"])
 
 def showDirections():
     print ("\n----------------------------------")
-    if "north" in rooms[currentRoom]:
-        previewRoom = rooms[currentRoom]["north"]
-        print ("To the north is the " + rooms[previewRoom]["name"])
-    if "south" in rooms[currentRoom]:
-        previewRoom = rooms[currentRoom]["south"]
-        print ("To the south is the " + rooms[previewRoom]["name"])
-    if "east" in rooms[currentRoom]:
-        previewRoom = rooms[currentRoom]["east"]
-        print ("To the east is the " + rooms[previewRoom]["name"])
-    if "west" in rooms[currentRoom]:
-        previewRoom = rooms[currentRoom]["west"]
-        print ("To the west is the " + rooms[previewRoom]["name"])
+    if "north" in location[currentLocation]:
+        previewLocation = location[currentLocation]["north"]
+        print ("To the north is the " + location[previewLocation]["name"])
+    if "south" in location[currentLocation]:
+        previewLocation = location[currentLocation]["south"]
+        print ("To the south is the " + location[previewLocation]["name"])
+    if "east" in location[currentLocation]:
+        previewLocation = location[currentLocation]["east"]
+        print ("To the east is the " + location[previewLocation]["name"])
+    if "west" in location[currentLocation]:
+        previewLocation = location[currentLocation]["west"]
+        print ("To the west is the " + location[previewLocation]["name"])
     print ("----------------------------------")
 
 def showInventory():
     print ("\n----------------------------------")
     if len(inventory) > 0:
         print (str(inventory))
+        #change this to printing each element in a normal string.
     else:
         print ("Your inventory is empty")
     print ("----------------------------------")
 
-rooms = {
-    1 : { "name" : "Road",
-          "east" : 2,
-          "south" : 3 },
+def showCharacter():
+    print ("\n----------------------------------")
+    print ("HP: %d/%d" % (currentHP, maxHP))
+    print ("Gold: %d" % currentGold)
+    print ("----------------------------------")
 
-    2 : { "name" : "Cave - Upper area",
-          "west" : 1,
-          "south" : 4,
-          "item" : "sword" },
+location = {
+    1: {"name": "The Kings Road",
+        "description": "Placeholder",
+        "east": 2,
+        "south": 3},
 
-    3 : { "name" : "Tavern",
-          "north" : 1,
-          "item" : "beer" },
+    2: {"name": "Cave - Upper area",
+        "description": "Placeholder",
+        "west": 1,
+        "south": 4,
+        "item": {"name": "sword",
+                 "description": "A rusty sword"},
+        },
 
-    4 : { "name" : "Cave - Area area",
-          "north" : 2 }
+    3: {"name": "The Black Horse Tavern",
+        "description": "Placeholder",
+        "north": 1,
+        "item": {"name": "beer",
+                 "description": "A foaming mug of ale"},
+        },
+
+    4: {"name": "Cave - Lower Area",
+        "description": "Placeholder",
+        "north": 2}
+
     }
 
-currentRoom = 1
-previewRoom = 0
+currentLocation = 1
+previewLocation = 0
 inventory = []
 showInstructions()
+currentHP = 10
+maxHP = 10
+currentGold = 10
 
 while True:
     showStatus()
@@ -68,23 +89,26 @@ while True:
     move = raw_input(">").lower().split()
 
     if move[0] == "go":
-        if move[1] in rooms[currentRoom]:
-            currentRoom = rooms[currentRoom] [move[1]]
+        if move[1] in location[currentLocation]:
+            currentLocation = location[currentLocation][move[1]]
         else:
             print ("You cannot go that way")
     elif move[0] == "get":
-        if "item" in rooms[currentRoom] and move[1] in rooms[currentRoom]["item"]:
+        if "item" in location[currentLocation] and move[1] in location[currentLocation]["item"]["name"]:
             inventory += [move[1]]
             print ("You picked up the " + move[1] + "!")
-            del rooms[currentRoom]["item"]
+            del location[currentLocation]["item"][move[1]]
         else:
             print("Cannot pickup " + move[1] + "!")
     elif move[0] == "map":
         showDirections()
     elif move[0] == "inventory":
         showInventory()
+    elif move[0] == "sheet":
+        showCharacter()
+    elif move[0] == "instructions":
+        showInstructions()
     elif move[0] == "quit" or move[0] == "exit":
         quit()
     else:
-        print ("\nNot a valid move!\n")
-        showInstructions()
+        print ("\nNot a valid move! Type 'instructions' to see valid moves.\n")
