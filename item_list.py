28175a1bf1be623d0b3be9bb19f591c23d9b1886
Robert Wilson <robwilson101@gmail.com>
2015-05-13 07:47:35 +0100

    Potential way to properly object items and call them

--- item_list.py
+++ item_list.py
@@ -8,3 +8,20 @@ ID           Name              Value
 Monsters
 ID           Name
 1            weak goblin
+
+
+class Items(object):
+    def __init__(self, iid):
+        self.iid = iid
+
+    def item_list(self, iid):
+        all_items = {
+            1: {"name" : "Sword",
+                "desc": "A rusty looking sword",
+                "dmg": 5,
+                "arm": 1,
+                "val": 10},
+            }
+
+        return all_items[iid]
+        #When calling items inventory.add_item(Items.item_list(iid))
